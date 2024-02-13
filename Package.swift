2daf87e892ccd4c0d64378be1002caddf5238343
runner <runner@Mac-1707812373227.local>
2024-02-13 08:43:39 +0000

    KMM SPM package release for 0.1.7

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/tddworks/openai-kotlin/com/tddworks/openai-client-darwin-kmmbridge/0.1.7/openai-client-darwin-kmmbridge-0.1.7.zip"
+let remoteKotlinChecksum = "9773e9d9968999c57a4713a57aef95c84131924e15d387b66ba4927426525317"
+let packageName = "openai-client-darwin"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
