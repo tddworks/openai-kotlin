2e50c7510987b525236d55a8a18263746c44fd2a
slam <slamhan1987@gmail.com>
2024-02-28 19:50:34 +0800

    feat: prepare for publish to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
 
 sonatypePortalPublisher {
     authentication {
-        username = "Uh+r/1by"
-        password = "vcjKVgS1D3xtqhpLNRu1smPLqr73R4Aw2nRJAqlLISvA"
+        username = ""
+        password = ""
     }
     settings {
         autoPublish = false
@@ -49,42 +49,6 @@ subprojects {
     apply(plugin = rootProject.libs.plugins.kotlinMultiplatform.get().pluginId)
     apply(plugin = rootProject.libs.plugins.build.dokka.plugin.get().pluginId)
 }
-
-//publishing {
-//    // Configure all publications
-//    publications.withType<MavenPublication> {
-//        // Stub javadoc.jar artifact
-//        artifact(tasks.register("${name}JavadocJar", Jar::class) {
-//            archiveClassifier.set("javadoc")
-//            archiveAppendix.set(this@withType.name)
-//        })
-//
-//        // Provide artifacts information required by Maven Central
-//        pom {
-//            name.set("Kotlin Multiplatform library template")
-//            description.set("Dummy library to test deployment to Maven Central")
-//            url.set("https://github.com/Kotlin/multiplatform-library-template")
-//
-//            licenses {
-//                license {
-//                    name.set("MIT")
-//                    url.set("https://opensource.org/licenses/MIT")
-//                }
-//            }
-//            developers {
-//                developer {
-//                    id.set("JetBrains")
-//                    name.set("JetBrains Team")
-//                    organization.set("JetBrains")
-//                    organizationUrl.set("https://www.jetbrains.com")
-//                }
-//            }
-//            scm {
-//                url.set("https://github.com/Kotlin/multiplatform-library-template")
-//            }
-//        }
-//    }
-//}
 
 koverReport {
     filters {
