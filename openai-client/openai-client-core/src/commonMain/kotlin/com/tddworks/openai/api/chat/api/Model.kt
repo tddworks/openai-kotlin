e8f6b45d06927e43c34b8f8e63a0ca755d96236c
slam <slamhan1987@gmail.com>
2024-04-06 12:44:12 +0800

    chore - refactor add doc

--- openai-client/openai-client-core/src/commonMain/kotlin/com/tddworks/openai/api/chat/api/Model.kt
+++ openai-client/openai-client-core/src/commonMain/kotlin/com/tddworks/openai/api/chat/api/Model.kt
@@ -10,7 +10,16 @@ value class Model(val value: String) {
         val GPT_3_5_TURBO = Model("gpt-3.5-turbo")
         val GPT_3_5_TURBO_0125 = Model("gpt-3.5-turbo-0125")
         val GPT_4 = Model("gpt-4")
+
+        /**
+         * GPT-4 Turbo
+         * The latest GPT-4 model intended to reduce cases of “laziness” where the model doesn’t complete a task. Returns a maximum of 4,096 output tokens.
+         */
         val GPT_4_TURBO_PREVIEW = Model("gpt-4-0125-preview")
+
+        /**
+         * Currently points to gpt-4-0125-preview.
+         */
         val GPT_4_TURBO = Model("gpt-4-turbo-preview")
         val GPT4_VISION_PREVIEW = Model("gpt-4-vision-preview")
         val DALL_E_2 = Model("dall-e-2")
