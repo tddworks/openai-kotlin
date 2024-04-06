e1b952d61ce12f131b994b604a34b2ebbf56af2d
slam <slamhan1987@gmail.com>
2024-04-06 16:28:29 +0800

    refactor: add ut

---
+++ openai-client/openai-client-core/src/jvmTest/kotlin/com/tddworks/openai/api/chat/api/ModelTest.kt
@@ -0,0 +1,26 @@
+package com.tddworks.openai.api.chat.api
+
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ModelTest {
+
+    @Test
+    fun `should return model from value`() {
+        assertEquals(Model.GPT_3_5_TURBO, Model("gpt-3.5-turbo"))
+        assertEquals(Model.GPT_3_5_TURBO_0125, Model("gpt-3.5-turbo-0125"))
+    }
+
+    @Test
+    fun `should return correct model values`() {
+        assertEquals("gpt-3.5-turbo", Model.GPT_3_5_TURBO.value)
+        assertEquals("gpt-3.5-turbo-0125", Model.GPT_3_5_TURBO_0125.value)
+        assertEquals("gpt-4", Model.GPT_4.value)
+        assertEquals("gpt-4-0125-preview", Model.GPT_4_TURBO_PREVIEW.value)
+        assertEquals("gpt-4-turbo-preview", Model.GPT_4_TURBO.value)
+        assertEquals("gpt-4-vision-preview", Model.GPT4_VISION_PREVIEW.value)
+        assertEquals("dall-e-2", Model.DALL_E_2.value)
+        assertEquals("dall-e-3", Model.DALL_E_3.value)
+    }
+}
