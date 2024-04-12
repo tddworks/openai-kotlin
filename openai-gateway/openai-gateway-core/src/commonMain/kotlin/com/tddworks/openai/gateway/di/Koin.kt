a2a617cd14dbd05c69a8efeb6448d526d501a0d6
slam <slamhan1987@gmail.com>
2024-04-12 23:18:44 +0800

    feat(BE-188): As a user, I want able to use claude api(anthropic-client) code refactor and introduce initOpenAIGatewayKoin

--- openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/di/Koin.kt
+++ openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/di/Koin.kt
@@ -3,9 +3,16 @@ import com.tddworks.anthropic.di.anthropicModules
 import com.tddworks.di.commonModule
 import com.tddworks.openai.api.OpenAIConfig
 import com.tddworks.openai.di.openAIModules
+import com.tddworks.openai.gateway.api.AnthropicOpenAIProvider
+import com.tddworks.openai.gateway.api.OpenAIGateway
+import com.tddworks.openai.gateway.api.OpenAIProvider
+import com.tddworks.openai.gateway.api.internal.DefaultOpenAIGateway
+import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
 
+@ExperimentalSerializationApi
 fun initOpenAIGatewayKoin(
     openAIConfig: OpenAIConfig,
     anthropicConfig: AnthropicConfig,
@@ -15,6 +22,16 @@ fun initOpenAIGatewayKoin(
     modules(
         commonModule(false) +
                 anthropicModules(anthropicConfig) +
-                openAIModules(openAIConfig)
+                openAIModules(openAIConfig) +
+                openAIGatewayModules()
     )
+}
+
+@ExperimentalSerializationApi
+fun openAIGatewayModules() = module {
+    single<AnthropicOpenAIProvider> { AnthropicOpenAIProvider(get()) }
+
+    single { listOf<OpenAIProvider>(get<AnthropicOpenAIProvider>()) }
+
+    single<OpenAIGateway> { DefaultOpenAIGateway(get(), get()) }
 }
