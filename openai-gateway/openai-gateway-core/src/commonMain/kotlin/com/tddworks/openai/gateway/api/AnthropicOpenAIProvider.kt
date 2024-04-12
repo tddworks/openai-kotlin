57ca2362a07c742a21a5db0ecf30dd2e5e9ba110
slam <slamhan1987@gmail.com>
2024-04-12 18:27:09 +0800

    feat(BE-188): As a user, I want able to use claude api(anthropic-client) code refactor.

--- openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/api/AnthropicOpenAIProvider.kt
+++ openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/api/AnthropicOpenAIProvider.kt
@@ -67,8 +67,6 @@ class AnthropicOpenAIProvider(private val client: Anthropic) : OpenAIProvider {
      * {"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-3.5-turbo-0125", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}
      *
      */
-
-
     private fun StreamMessageResponse.toOpenAIChatCompletionChunk(model: String): OpenAIChatCompletionChunk {
         val id = "chatcmpl-123"
         val created = 1L
