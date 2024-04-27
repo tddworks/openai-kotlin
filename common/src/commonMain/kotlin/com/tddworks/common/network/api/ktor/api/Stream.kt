d186983039dc91e813ff820f4756780434eebece
slam <slamhan1987@gmail.com>
2024-04-27 16:16:59 +0800

    refactor: improve streaming event handling
    - Refactored the streaming event handling logic for better readability and efficiency, including simplifying conditions and renaming functions for clarity.

--- common/src/commonMain/kotlin/com/tddworks/common/network/api/ktor/api/Stream.kt
+++ common/src/commonMain/kotlin/com/tddworks/common/network/api/ktor/api/Stream.kt
@@ -15,18 +15,16 @@ private const val STREAM_END_TOKEN = "$STREAM_PREFIX [DONE]"
  */
 suspend inline fun <reified T> FlowCollector<T>.streamEventsFrom(response: HttpResponse) {
     val channel: ByteReadChannel = response.body()
-
-    // Continue to read until the channel is closed.
+    val json = json()
     while (!channel.isClosedForRead) {
-        channel.readUTF8Line()?.let { streamResponse ->
-            if (notEndStreamResponse(streamResponse)) {
-                // If the response indicates streaming data, decode and emit it.
-                emit(json().decodeFromString<T>(streamResponse.removePrefix(STREAM_PREFIX)))
-            } else if (isJson(streamResponse)) {
-                // Ollama - response is a json object
-                emit(json().decodeFromString<T>(streamResponse))
-            }
-        } ?: break // If `readUTF8Line()` returns null, exit the loop (end of input).
+        val line = channel.readUTF8Line() ?: continue
+        val value: T = when {
+            endStreamResponse(line) -> break
+            isStreamResponse(line) -> json.decodeFromString(line.removePrefix(STREAM_PREFIX))  // If the response indicates streaming data, decode and emit it.
+            isJsonResponse(line) -> json.decodeFromString(line) // Ollama - response is a json object without `data:` prefix
+            else -> continue
+        }
+        emit(value)
     }
 }
 
@@ -36,8 +34,8 @@ fun json(): Json {
 }
 
 
-private fun isStreamResponse(line: String) = line.startsWith(STREAM_PREFIX)
+fun isStreamResponse(line: String) = line.startsWith(STREAM_PREFIX)
 
-fun notEndStreamResponse(line: String) = line != STREAM_END_TOKEN && isStreamResponse(line)
+fun endStreamResponse(line: String) = line.startsWith(STREAM_END_TOKEN)
 
-fun isJson(line: String) = line.startsWith("{") && line.endsWith("}")
+fun isJsonResponse(line: String) = line.startsWith("{") && line.endsWith("}")
