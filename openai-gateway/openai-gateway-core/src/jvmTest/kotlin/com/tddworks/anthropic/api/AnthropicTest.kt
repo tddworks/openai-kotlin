98ea4e4d6aebc0d23d199ae53dd22caa8fd2d061
slam <slamhan1987@gmail.com>
2024-06-04 19:56:05 +0800

    test(ut): add AnthropicTest for anthropic initialization logic

---
+++ openai-gateway/openai-gateway-core/src/jvmTest/kotlin/com/tddworks/anthropic/api/AnthropicTest.kt
@@ -0,0 +1,26 @@
+package com.tddworks.anthropic.api
+
+import com.tddworks.anthropic.di.iniAnthropic
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.test.junit5.AutoCloseKoinTest
+
+
+class AnthropicTest : AutoCloseKoinTest() {
+
+    @BeforeEach
+    fun setUp() {
+        iniAnthropic(config = AnthropicConfig())
+    }
+
+    @Test
+    fun `should create anthropic with default settings`() {
+
+        val anthropic = Anthropic()
+
+        assertEquals("CONFIGURE_ME", anthropic.apiKey())
+        assertEquals(Anthropic.BASE_URL, anthropic.baseUrl())
+        assertEquals("2023-06-01", anthropic.anthropicVersion())
+    }
+}
