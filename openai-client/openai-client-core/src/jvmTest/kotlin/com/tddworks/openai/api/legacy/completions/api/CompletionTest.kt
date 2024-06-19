2c2c780d02a5d0bc9424e583802a51fd8c53fdbc
slam <slamhan1987@gmail.com>
2024-06-19 23:37:16 +0800

    feat(BE-215): ollama fim api support  - add missing ut

--- openai-client/openai-client-core/src/jvmTest/kotlin/com/tddworks/openai/api/legacy/completions/api/CompletionTest.kt
+++ openai-client/openai-client-core/src/jvmTest/kotlin/com/tddworks/openai/api/legacy/completions/api/CompletionTest.kt
@@ -7,6 +7,47 @@ import org.junit.jupiter.api.Test
 class CompletionTest {
 
     @Test
+    fun `should return dummy completion`() {
+        val completion = Completion.dummy()
+
+        with(completion) {
+            assertEquals("id", id)
+            assertEquals(0, created)
+            assertEquals("model", model)
+            assertEquals("systemFingerprint", systemFingerprint)
+            assertEquals("object", `object`)
+            assertNotNull(usage)
+        }
+    }
+
+    @Test
+    fun `should return completion with required fields`() {
+        val completion = Completion(
+            id = "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
+            choices = listOf(
+                CompletionChoice(
+                    text = "\n\nThis is indeed a test",
+                    index = 0
+                )
+            ),
+            created = 1589478378,
+            model = "gpt-3.5-turbo-instruct"
+        )
+
+        with(completion) {
+            assertEquals("cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7", id)
+            assertEquals(1589478378, created)
+            assertEquals("gpt-3.5-turbo-instruct", model)
+            assertEquals(1, choices.size)
+            assertEquals("\n\nThis is indeed a test", choices[0].text)
+            assertEquals(0, choices[0].index)
+            assertEquals("", choices[0].finishReason)
+            assertNull(choices[0].logprobs)
+            assertNull(usage)
+        }
+    }
+
+    @Test
     fun `should return completion`() {
         val completionJson = """
             {
