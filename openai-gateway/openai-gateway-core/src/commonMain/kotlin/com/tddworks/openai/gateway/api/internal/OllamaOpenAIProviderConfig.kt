2e400fb2999e11f2267c28f0a6be70514c68ab6c
slam <slamhan1987@gmail.com>
2025-01-03 16:21:06 +0800

    feat(ollama): add port and protocol to provider config
    Include port and protocol configuration options in the OllamaOpenAIProviderConfig class to enhance flexibility in specifying network settings. Modify related functions accordingly for seamless integration.

--- openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/api/internal/OllamaOpenAIProviderConfig.kt
+++ openai-gateway/openai-gateway-core/src/commonMain/kotlin/com/tddworks/openai/gateway/api/internal/OllamaOpenAIProviderConfig.kt
@@ -4,14 +4,18 @@ import com.tddworks.ollama.api.OllamaConfig
 import com.tddworks.openai.gateway.api.OpenAIProviderConfig
 
 data class OllamaOpenAIProviderConfig(
+    val port: () -> Int = { 11434 },
+    val protocol: () -> String = { "http" },
     override val baseUrl: () -> String = { "http//:localhost:11434" },
     override val apiKey: () -> String = { "ollama-ignore-this" }
 ) : OpenAIProviderConfig
 
 fun OllamaOpenAIProviderConfig.toOllamaConfig() =
-    OllamaConfig(baseUrl = baseUrl)
+    OllamaConfig(baseUrl = baseUrl, protocol = protocol, port = port)
 
 fun OpenAIProviderConfig.Companion.ollama(
     apiKey: () -> String = { "ollama-ignore-this" },
-    baseUrl: () -> String = { "http//:localhost:11434" },
-) = OllamaOpenAIProviderConfig(baseUrl, apiKey)
+    baseUrl: () -> String = { "localhost" },
+    protocol: () -> String = { "http" },
+    port: () -> Int = { 11434 }
+) = OllamaOpenAIProviderConfig(port, protocol, baseUrl, apiKey)
