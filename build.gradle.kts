7f2fd27898907ff9ea4189eb59179755fae43c78
slamhan <slamhan1987@hotmail.com>
2025-08-23 12:21:25 +0800

    refactor: Code Formatting with Spotless and update to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,12 +89,12 @@ centralPublisher {
     
     projectInfo {
         name = "openai-kotlin"
-        description = "penai-kotlin powered by kotlin multiplatform"
+        description = "OpenAI API KMP Client"
         url = "https://github.com/tddworks/openai-kotlin"
         
         license {
             name = "The Apache Software License, Version 2.0"
-            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            url = "https://github.com/tddworks/openai-kotlin/blob/main/LICENSE"
         }
         
         developer {
