d6f360128a8bea7014bce387d96298c06d0e0a89
slamhan <slamhan1987@hotmail.com>
2025-12-18 10:07:17 +0800

    build(kmmbridge): switch to gitHubReleaseArtifacts
    - Replace mavenPublishArtifacts and GitHub Packages repo with gitHubReleaseArtifacts in all Darwin clients
    - Add KMMBridge-Release and KMMBridge-Debug GitHub Actions for artifact publishing

--- openai-gateway/openai-gateway-darwin/build.gradle.kts
+++ openai-gateway/openai-gateway-darwin/build.gradle.kts
@@ -31,18 +31,8 @@ kotlin {
     }
 }
 
-addGithubPackagesRepository() // <- Add the GitHub Packages repo
-
 kmmbridge {
-    /**
-     * reference: https://kmmbridge.touchlab.co/docs/artifacts/MAVEN_REPO_ARTIFACTS#github-packages
-     * In kmmbridge, notice mavenPublishArtifacts() tells the plugin to push KMMBridge artifacts to
-     * a Maven repo. You then need to define a repo. Rather than do everything manually, you can
-     * just call addGithubPackagesRepository(), which will add the correct repo given parameters
-     * that are passed in from GitHub Actions.
-     */
-    mavenPublishArtifacts() // <- Publish using a Maven repo
-    /** https://github.com/touchlab/KMMBridge/pull/251 - Multiple packages in same Package.swift */
+    gitHubReleaseArtifacts()
     spm(swiftToolVersion = "5.9", useCustomPackageFile = true, perModuleVariablesBlock = true) {
         iOS { v("15") }
         macOS { v("15") }
